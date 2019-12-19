9a7d8d60974a6db21fb843ead56f7354af8fed00
sahilsharma884 <49679122+sahilsharma884@users.noreply.github.com>
2019-12-19 14:04:55 +0530

    Update Code.py

--- Project Euler/Pr_2/Code.py
+++ Project Euler/Pr_2/Code.py
@@ -7,15 +7,23 @@ import sys
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
+    # Initialize first and second value to generate Fibonacci's series
     a = 1
     b = 2
+    # Since initializing two number, one of them is even (b=2).
+    # So, we initialize with starting sum_ as 2
     sum1 = 2
+    # Fibonacci's series works as take previous two input and put into third variable
     c = a + b
+    # Generate fibonacci's series until less than 'n'
     while(c < n):
+        # If third variable is even, then add them. That's our task.
+        # Task: Add those number which are even
         if (c % 2 == 0):
             sum1 += c
+        # The next three line is that how Fibonacci's series works
         a = b
         b = c
         c = a + b
-    
+    # Generate output of sum_
     print(sum1)
