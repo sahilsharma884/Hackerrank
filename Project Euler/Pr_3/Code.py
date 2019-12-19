d6b4f8247c52a152d2fd904c7a5df6203d85c029
sahilsharma884 <49679122+sahilsharma884@users.noreply.github.com>
2019-12-19 14:09:02 +0530

    Update Code.py

--- Project Euler/Pr_3/Code.py
+++ Project Euler/Pr_3/Code.py
@@ -4,6 +4,8 @@
 import sys
 import math
 
+# Define function to check whether given number is prime number or not?
+# This function is optimized ones.
 def isprime(number):
     if number == 2 or number == 3:
         return True
@@ -21,14 +23,20 @@ def isprime(number):
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
+    # Initialize with max_ variable = 0 to store the largest prime factor.
     max_ = 0
+    # Please see the README.md to understand workflow and scenario
+    # Scenario 1
     if isprime(n):
         max_ = n
     else:
         for i in range(2,math.floor(math.sqrt(n))+1):
             if n % i == 0:
+                # Scenario 2
                 if isprime(i):
                     max_ = i
+                # Scenario 3
                 if isprime(n//i) and max_ < n//i:
                     max_ = n//i
+    # Generate output of largest prime factor
     print(max_)
